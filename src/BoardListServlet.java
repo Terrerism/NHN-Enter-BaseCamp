80928b9f93ecbfe5f8edb0e630f25ecfe910b65f
Terrerism <show9314@gmail.com>
2016-12-31 01:40:21 +0900

    Add files via upload
    DB에 있는 내용을 출력

---
+++ src/BoardListServlet.java
@@ -0,0 +1,62 @@
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Created by show9 on 2016-12-31.
+ */
+@WebServlet("/board/list")
+public class BoardListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            ServletContext servletContext = this.getServletContext();
+            Class.forName(servletContext.getInitParameter("driver"));
+            connection = DriverManager.getConnection(
+                    servletContext.getInitParameter("url"),
+                    servletContext.getInitParameter("username"),
+                    servletContext.getInitParameter("password"));
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(
+                    "select EMAIL,CONTENT,CRE_TIME,MOD_TIME" +
+                            " from BOARDS" +
+                            " order by CRE_TIME ASC");
+            response.setContentType("text/html; charset=UTF-8");
+            PrintWriter out = response.getWriter();
+            out.println("<html><head><title>방명록</title></head>");
+            out.println("<body><h1>방명록</h1>");
+            int num = 1;
+            while (resultSet.next()) {
+                out.println(
+                        num + "," +
+                        resultSet.getString("EMAIL") + "," +
+                        resultSet.getString("CONTENT") + "," +
+                        resultSet.getTimestamp("CRE_TIME") + "," +
+                        resultSet.getTimestamp("MOD_TIME") + "<br>");
+                num++;
+            }
+            out.println("</body></html>");
+        } catch (Exception e) {
+            throw new ServletException(e);
+        } finally {
+            try {if (resultSet != null) resultSet.close();} catch (Exception e) {}
+            try {if (statement != null) statement.close();} catch (Exception e) {}
+            try {if (connection != null) connection.close();} catch (Exception e) {}
+        }
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: show9
+  Date: 2016-12-30
+  Time: 오후 11:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>타이틀</title>
+  </head>
+  <body>
+  안녕??
+  </body>
+</html>
